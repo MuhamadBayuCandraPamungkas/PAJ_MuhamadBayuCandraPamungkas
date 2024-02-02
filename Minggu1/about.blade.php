32c6b93d3d148badac7404edb9c382072bc3b903
Muhamad Bayu Candra Pamungkas <82008636+MuhamadBayuCandraPamungkas@users.noreply.github.com>
2024-02-02 10:10:40 +0700

    Update

--- Minggu1/about.blade.php
+++ Minggu1/about.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="with=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Services</title>
+    <title>About</title>
 </head>
 <body>
-    <h1>Ini Halaman Services</h1>
-    <down>Ayo Belajar Laravel</down>
+    <h1>Ini Halaman About</h1>
+    <down>Belajar Laravel Selalu Menyenangkan</down>
 </body>
 </html>
