eec4b337642904e5ce849bf09501b4c15f49f655
Muhamad Bayu Candra Pamungkas <82008636+MuhamadBayuCandraPamungkas@users.noreply.github.com>
2024-03-07 14:37:25 +0700

    Upload

---
+++ Minggu7/resources/views/task/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <h1> Task List</h1>
+
+        @foreach ($tasks as $task)
+            <div class="task-item">
+                <strong>Name : {{ $task->name }}</strong>
+                <p>Description : {{$task->description}}</p>
+
+                <a href="{{url("/task/{$task->id}")}}" class="view-link">View</a>
+                <a href="{{url("/task/{$task->id}/edit")}}" class="view-link">Edit</a>
+
+                <form action="{{url('/task' ,[$task->id])}}" method="POST" class="delete-form">
+                    @method('delete')
+                    @csrf
+                    <button type="submit">DELETE</button>
+</form>
+</div>
+@endforeach
+</div>
+@endsection
